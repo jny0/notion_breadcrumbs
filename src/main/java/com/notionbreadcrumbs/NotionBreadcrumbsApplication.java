8990a67e7106654dc394ae22e78aaa34c95af747
jny0 <pjy100402@naver.com>
2023-09-04 06:30:25 +0900

    Feat : init

---
+++ src/main/java/com/notionbreadcrumbs/NotionBreadcrumbsApplication.java
@@ -0,0 +1,13 @@
+package com.notionbreadcrumbs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotionBreadcrumbsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotionBreadcrumbsApplication.class, args);
+	}
+
+}
